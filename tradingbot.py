ca2ac80de77cf6ad0362bcd91136012f190def1f
Ben Hsieh <bqh5026@gmail.com>
2024-12-10 23:16:08 -0600

    Update tradingbot file

--- tradingbot.py
+++ tradingbot.py
@@ -1,3 +1,15 @@
-from datetime import datetime
 from lumibot.brokers import Alpaca
 from lumibot.backtesting import YahooDataBacktesting 
+from lumibot.strategies.strategy import Strategy
+from lumibot.traders import Trader
+from datetime import datetime
+
+API_KEY = ""
+API_SECRET = ""
+BASE_URL = "https://paper-api.alpaca.markets"
+
+ALPACA_CREDS = {
+    "API_KEY": API_KEY,
+    "API_SECRET": API_SECRET, 
+    "PAPER": True
+}
